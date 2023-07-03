c3491fcd1c74f4200b3a58ff22a301f232405154
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-07-03 09:36:54 -0400

    Add survey questions

--- survey.js
+++ survey.js
@@ -5,8 +5,24 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+// Example of how to use readline module:
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
 
-  rl.close();
+//   rl.close();
+// });
+
+rl.question("What is your name? ", (name) => {
+  rl.question("What are your pronouns? ", (pronouns) => {
+    rl.question("Where do you live? ", (location) => {
+      rl.question("What is your favourite breakfast food? ", (food) => {
+        rl.question("What is the name if your favourite book? ", (book) => {
+          rl.question("Tell us one interesting thing about you: ", (thing) => {
+            rl.close();
+            console.log(`Hi! My name is ${name} and my pronouns are ${pronouns}. I live in ${location}. My absolute favourite breakfast food is ${food}. My favourite book is ${book}. ${thing}.`);
+          })
+        })
+      })
+    })
+  })
 });
